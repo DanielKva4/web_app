f9849577195366023d5db16cccee2eb87c8b5f0f
DanielKva4 <83667857+DanielKva4@users.noreply.github.com>
2021-05-23 00:11:30 +0300

    login system work~

--- app/views.py
+++ app/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from app.models import *
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
-    return HttpResponse("Это главная страница!")
+    if request.user.is_authenticated:
+        return render(request, 'start_page_root.html')
+    else:
+        return render(request, 'start_page.html')
 
 
-def first_person(request):
-    x = Person.objects.all()
-    return HttpResponse(x[0].name)
+def about(request):
+    return render(request, 'about.html')
+
+
+def login_page(request):
+    return render(request, 'login.html')
+
+
+def error(request):
+    return render(request, 'error.html')
+
+
+def login_user(request):
+    user = authenticate(
+        username=request.POST['username'],
+        password=request.POST['password']
+    )
+    if user is None:
+        return render(request, 'error.html')
+    else:
+        login(request, user)
+        return HttpResponseRedirect('/')
+
+
+
+# def start_page(request):
+#     if request.user.is_authenticated:
+#         return render(request, 'start_page_root.html')
+#     else:
+#         return render(request, 'start_page.html')
+
