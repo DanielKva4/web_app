1bf217bbc18071af70cd88d2cc90a02a3a6f2978
DanielKva4 <83667857+DanielKva4@users.noreply.github.com>
2021-06-27 01:15:41 +0300

    Готова вьюха среднего курса за неделю

--- app/ForTests.py
+++ app/ForTests.py
@@ -1,3 +1,14 @@
+import numpy as np
+import pandas
+from sklearn import linear_model
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+import requests
+import json
+from datetime import date, timedelta
+import statistics
+
+
 # def shame():
 #     return 'Твоя функция не пашет дружок'
 #
@@ -10,16 +21,8 @@
 #         return shame()
 
 
-
-import numpy as np
-import pandas
-from sklearn import linear_model
-from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
-from pandas import DataFrame
-
-# def neironka():
-#     df = pandas.read_csv('/home/user/csv/iq.csv')
+# def linear_regression():
+#     df = pandas.read_csv('D:/Учеба/Базы/iq.csv')  # Для Linux /home/user/csv/iq.csv
 #     years_of_education = df['years_of_education'][:, np.newaxis]
 #     x_train, x_test, y_train, y_test = train_test_split(
 #         years_of_education, df['iq'], test_size=0.4
@@ -33,20 +36,20 @@ from pandas import DataFrame
 #     print('Точность: %s' % score)
 #     return
 
-import requests
-import json
 
 
 def curses():
+    link = 'https://www.nbrb.by/api/exrates/rates/dynamics/145?startDate=' + str(date.today() - timedelta(days=7)) + '&enddate=' + str(date.today())
     response = requests.get(
-        'https://www.nbrb.by/api/exrates/rates/dynamics/145?startDate=2021-06-16&enddate=2021-06-25'
+        link
     )
     data = json.loads(response.text)
     len_days = len(data)
-    curse = 0
+    curse = []
     for x in range(len_days):
-        curse += data[x]['Cur_OfficialRate']
-    result = curse / len_days
+        curse.append(data[x]['Cur_OfficialRate'])
+    result = statistics.mean(curse)
+    result = float('{:.4f}'.format(result))
     return result
 
 
